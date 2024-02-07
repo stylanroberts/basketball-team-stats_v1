1734520638a39efbf1e494e92b23b2ef0da681a7
Stylan Roberts <stylanroberts@icloud.com>
2024-02-07 10:31:22 -0600

    some progress

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import copy
 from constants import PLAYERS, TEAMS
 
 
+def balance_teams(teams_copy, clean_list):
+    teams_copy = copy.deepcopy(teams_copy)
+    for team in teams_copy:
+        team = []
+        player_name = [name["name"] for name in clean_list]
+        if len(team) < 6:
+            team.append(player_name)
+            del player_name
+    print(team)
+
+
 
 def stats_tool():
     print("BASKETBALL STATS TOOL\n")
@@ -27,38 +38,50 @@ def start_or_quit():
         start_or_quit()
 
 
-def choose_team():
-    for team in TEAMS:
-        print(f"\n{int(TEAMS.index(team))+1}) { team}")
-    print(range(len(TEAMS)))
-    response = int(input("\nChoose a team:    "))
-    if response in range(1, len(TEAMS)):
-        print(f"Team {team[response+1]}")
+# def choose_team():
+#     # for team in TEAMS:
+#     clean_data(PLAYERS)
+#     for index, team in enumerate(TEAMS, 1):
+#         print(f"{index}) {team}")
+#         # print(f"\n{int(TEAMS.index(team))+1}) { team}")
+#     response = int(input("\nChoose a team:    "))
+#     picked_team = TEAMS[response-1]
+#     if response in range(1, len(TEAMS)):
+#         print(f"\nTeam {picked_team} Stats")
+#     if picked_team == TEAMS[0]:
 
+#     else: 
+#         print("That is not a choice. Try again")
+#     print("\n----------")
+#     print(f"Total players: {len(picked_team)}")
+#     print("\nPlayers on team:")
+#     print(f"\n{picked_team}")
+   
+    # function that takes picked team and shows stats or players or something like that
+    #
 
-def player_to_team():
-    for team in TEAMS:
-        for player in PLAYERS:
-            # for team in TEAMS:
-                team = []
-                players_copy = copy.deepcopy(PLAYERS)
-                # players_list = PLAYERS.split(",")
-                player = [name.get("name") for name in PLAYERS]
-                if len(team) < ( len(PLAYERS) // len(TEAMS) ):
-                    sorted_team = team.append(player)
-                else:
-                    continue
-                    
+# change this
+def clean_data(players):
+    cleaned = []
+    players = copy.deepcopy(players)
+    for user in players:
+        fixed = {}
+        fixed['name'] = user['name']
+        
+        fixed['height'] = int(user['height'].split(' ')[0])
+        if user['experience'.lower()] == "yes":
+            fixed['experience'] = True
+        else:
+            fixed['experience'] = False
+        cleaned.append(fixed)
 
-                    
-
-
-
-        print(sorted_team)
+    return cleaned
 
 
 if __name__ == "__main__":
-    player_to_team()
+    # balance_teams(TEAMS, clean_data(PLAYERS))
+    print(clean_data(PLAYERS))
     stats_tool()
+
 
 
