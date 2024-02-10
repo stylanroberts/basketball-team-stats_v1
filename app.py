25f45d3c5314c11b7cdf7ab17a86ed8ec6738c26
Stylan Roberts <stylanroberts@icloud.com>
2024-02-10 01:36:59 -0600

    more functions

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 import copy
 from constants import PLAYERS, TEAMS
 
+players = copy.deepcopy(PLAYERS)
 
-# def balance_teams(teams_copy, clean_list):
-#     teams_copy = copy.deepcopy(teams_copy)
-#     for team in teams_copy:
-#         team = []
-#         player_name = [name["name"] for name in clean_list]
-#         if len(team) < 6:
-#             team.append(player_name)
-#             del player_name
-#     print(team)
+team_length = len(PLAYERS) // len(TEAMS)
+
+panthers = []
+bandits = []
+warriors = []
 
 
+team_list = [panthers, bandits, warriors]
 
 def stats_tool():
     print("BASKETBALL STATS TOOL\n")
     print("\n----MENU----\n")
-    # function to start game
     start_or_quit()
-    # start_or_quit here maybe or might put in dunder
-
 
 def start_or_quit():
     print("\nHere are your options:\n")
@@ -37,37 +32,90 @@ def start_or_quit():
         print("\nNot a valid input, try again.")
         start_or_quit()
 
-def balanced_teams(teams, players):
-    players_copy = copy.deepcopy(players)
-    players_clean = clean_data(players_copy)
-    for team in teams:
-        team = []
-        for player in players_clean:
-            if len(team) == 0:
-                team.append(player)
-                players_clean.remove(player)
-            elif len(team) < len(players_clean) // len(team):
-                team.append(player)
-                players_clean.remove(player)
-        print(team)
-    return balanced_teams
+
+def balance_teams():
+    players_clean = clean_data(PLAYERS)
+    for player in players_clean:
+        if len(panthers) < team_length:
+            panthers.append(player)
+        elif len(bandits) < team_length:
+            bandits.append(player)
+        elif len(warriors) < team_length:
+            warriors.append(player)
+
+
+def get_name(team):
+    team_names = []
+    for player in team:
+        
+        player_name = player.get('name')
+        team_names.append(player_name)
+    print(team_names)
+    
+
+def get_stats(team):
+    print("\nWould you like to see the stats of any of this team's players?")
+    response = input("\nIf so, type in there first and last name, if not, press Q!    ")
+    if response.lower() == "q".lower():
+        print('Goodbye!')
+        exit()
+
+    for player in team:
+
+        player_height = player.get('height')
+        player_guardians = player.get('guardians')
+        player_name = player.get('name')
+
+        if response.title() not in player.values():
+            response = input("\nSorry they aren't on the team, try another player!:    ")
+
+
+        elif  response.title() in player.values():
+            print(f"\nStats for {player_name}:\n")
+            print(f"Height: {player_height}")
+            
+            print(f"Parent(s): {player_guardians}")
+            if player['experience'] == False:
+                print("Experience: low")
+            else:
+                print("Experience: high")
+            exit()
 
 
 
 def choose_team():
     for index, team in enumerate(TEAMS, 1):
         print(f"{index}) {team}")
-        # print(f"\n{int(TEAMS.index(team))+1}) { team}")
-    response = int(input("\nChoose a team:    "))
-    picked_team = TEAMS[response-1]
-    if response in range(1, len(TEAMS)):
-        print(f"\nTeam {picked_team} Stats:")
-    # if picked_team == TEAMS[0]:
+    
+    while True:
+        response = int(input("\nChoose a team:    "))-1
 
-    # else: 
-    #     print("That is not a choice. Try again")
-    # print("\n----------")
-    # print(f"Total players: {len(picked_team)}")
+        if response not in range(0, len(TEAMS)):
+            print("Out of range, try again.")
+        else:
+            break
+    picked_team = TEAMS[response]
+
+    # if response in range(0, len(TEAMS)):
+    print(f"\nTeam {picked_team}:")
+    print("\n----------")
+    print(f"Total players: {len(team_list[response])}")
+    # elif response not in range(0, len(TEAMS)):
+    #     response = int(input("Out of range, try again:    "))
+    if picked_team == TEAMS[0]:
+        
+        get_name(panthers)
+        get_stats(panthers)
+    elif picked_team == TEAMS[1]:
+        get_name(warriors)
+        get_stats(warriors)
+    elif picked_team == TEAMS[2]:
+        get_name(bandits)
+        get_stats(bandits)
+        
+    else: 
+        response = int(input("That is not a choice. Try again"))
+
     # print("\nPlayers on team:")
     # print(f"\n{picked_team}")
    
@@ -79,10 +127,11 @@ def clean_data(players):
     players = copy.deepcopy(players)
     for user in players:
         fixed = {}
+        fixed['guardians'] = user['guardians'].split('and')
         fixed['name'] = user['name']
 
         fixed['height'] = int(user['height'].split(' ')[0])
-        if user['experience'.lower()] == "yes":
+        if user['experience'] == "YES":
             fixed['experience'] = True
         else:
             fixed['experience'] = False
@@ -90,17 +139,6 @@ def clean_data(players):
 
     return cleaned
 
-
-
-
 if __name__ == "__main__":
-    # balanced_teams(TEAMS, PLAYERS)
-    balanced_teams(TEAMS, PLAYERS)
+    balance_teams()
     stats_tool()
-
-
-# def show_roster(team):
-#     team = []
-#     for player in team:
-#         player_name = player.get('name')
-#         team.append(player_name)
