49d5aae51e0b6e575ab98a345339842eb3416b1f
Stylan Roberts <stylanroberts@icloud.com>
2024-02-13 05:30:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,28 +9,46 @@ panthers = []
 bandits = []
 warriors = []
 
-# team_list =[]
-
-# maybe idk if this will work yet
-
-def sort_teams():
-    for team in TEAMS:
-        team = []
-        if len(team) < team_length:
-            for player in players:
-                team.append(player)
-                del player
-        team_list.append(team)
-    return team_list
-    
-
-
+experienced = []
+not_experienced = []
 team_list = [panthers, bandits, warriors]
+
+
+def exp_or_not(players):
+    players_clean = clean_data(players)
+    for player in players_clean:
+
+        if player.get('experience') == True:
+            experienced.append(player)
+        else:
+            not_experienced.append(player)
+
+
+def balance_teams():
+    exp_or_not(players)
+    exp_per_team = len(experienced) // len(TEAMS)
+    for player in experienced:
+        if len(panthers) < exp_per_team:
+            panthers.append(player)
+        elif len(bandits) < exp_per_team:
+           bandits.append(player)
+        elif len(warriors) < exp_per_team:
+            warriors.append(player)
+
+    for player in not_experienced:
+        if len(panthers) < team_length:
+            panthers.append(player)
+        elif len(bandits) < team_length:
+            bandits.append(player)
+        elif len(warriors) < team_length:
+            warriors.append(player)
+
 
 def stats_tool():
     print("BASKETBALL STATS TOOL\n")
     print("\n----MENU----\n")
     start_or_quit()
+
 
 def start_or_quit():
     print("\nHere are your options:\n")
@@ -48,40 +66,42 @@ def start_or_quit():
         start_or_quit()
 
 
-def balance_teams():
-    players_clean = clean_data(PLAYERS)
-    for player in players_clean:
-        if len(panthers) < team_length:
-            panthers.append(player)
-        elif len(bandits) < team_length:
-            bandits.append(player)
-        elif len(warriors) < team_length:
-            warriors.append(player)
-
-
 def get_name(team):
     team_names = []
     for player in team:
         
         player_name = player.get('name')
         team_names.append(player_name)
-    print(team_names)
+    for team in team_names:
+        print(team, end=', ')
     
 
 def get_stats(team):
     print("\nWould you like to see the stats of any of this team's players?")
-    response = input("\nIf so, type in there first and last name, if not, press Q!    ")
-    if response.lower() == "q".lower():
-        print('Goodbye!')
-        exit()
+    while True:
+        response = input("\nType in their first and last name. If you want to quit, press Q!    ")
+        player_response = response.title()
+        if response.lower() == "q".lower():
+            print('Goodbye!')
+            exit()
+        # this link helped explain how to check for key names in list of dicts
+        # https://stackoverflow.com/questions/3897499/check-if-value-already-exists-within-list-of-dictionaries-in-python
+        if not any(player['name'] == player_response for player in team):
+            print("Sorry not in the team, we need a player on THIS team!")
+        else:
+            break
 
     for player in team:
 
         player_height = player.get('height')
         player_guardians = player.get('guardians')
+
+        if len(player_guardians) > 1:
+            player_guardians = ', '.join(player_guardians)
+            
         player_name = player.get('name')
 
-        # if response.title() not in player.values():
+        # if response.title() not in player.values():a
         #     response = input("\nSorry they aren't on the team, try another player!:    ")
 
 
@@ -89,11 +109,11 @@ def get_stats(team):
             print(f"\nStats for {player_name}:\n")
             print(f"Height: {player_height}")
             
-            print(f"Parent(s): {player_guardians}")
+            print(f"Guardian(s): {player_guardians}")
             if player['experience'] == False:
-                print("Experience: low")
+                print("Experience: Low")
             else:
-                print("Experience: high")
+                print("Experience: High")
             exit()
 
 
@@ -109,40 +129,38 @@ def choose_team():
             print("Out of range, try again.")
         else:
             break
-    picked_team = TEAMS[response]
+    picked_team_name = TEAMS[response]
+    picked_team = team_list[response]
 
     # if response in range(0, len(TEAMS)):
-    print(f"\nTeam {picked_team}:")
+    print(f"\nTeam {picked_team_name}:")
     print("\n----------")
-    print(f"Total players: {len(team_list[response])}")
-    # elif response not in range(0, len(TEAMS)):
-    #     response = int(input("Out of range, try again:    "))
-    if picked_team == TEAMS[0]:
-        
-        get_name(panthers)
-        get_stats(panthers)
-    elif picked_team == TEAMS[1]:
-        get_name(warriors)
-        get_stats(warriors)
-    elif picked_team == TEAMS[2]:
-        get_name(bandits)
-        get_stats(bandits)
-        
-    else: 
-        response = int(input("That is not a choice. Try again"))
+    print(f"\nTotal players: {len(team_list[response])}")
 
-    # print("\nPlayers on team:")
-    # print(f"\n{picked_team}")
-   
-    # # function that takes picked team and shows stats or players or something like that
-    
+
+    experienced_num = 0
+    not_experienced_num = 0
+    for player in picked_team:
+
+        if player.get('experience') == True:
+            experienced_num+=1
+        else:
+            not_experienced_num+=1
+
+    print(f"\nNumber of experienced players: {experienced_num}\n")
+    get_name(picked_team)
+    get_stats(picked_team)
+
 
 def clean_data(players):
     cleaned = []
     players = copy.deepcopy(players)
     for user in players:
         fixed = {}
-        fixed['guardians'] = user['guardians'].split('and')
+        if len(user['guardians']) > 1:
+            fixed['guardians'] = user['guardians'].split(' and ')
+        else:
+            fixed['guardians'] = user['guardians']
         fixed['name'] = user['name']
 
         fixed['height'] = int(user['height'].split(' ')[0])
@@ -151,8 +169,8 @@ def clean_data(players):
         else:
             fixed['experience'] = False
         cleaned.append(fixed)
-
     return cleaned
+
 
 if __name__ == "__main__":
     balance_teams()
